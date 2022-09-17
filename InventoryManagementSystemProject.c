19c2a9c3be18cfe3981af5ebfc89c8764040b417
Mahi Maliha <87617184+MahiMaliha@users.noreply.github.com>
2022-09-17 18:16:41 +0600

    added variables & values

--- InventoryManagementSystemProject.c
+++ InventoryManagementSystemProject.c
@@ -3,3 +3,13 @@
 #include <stdlib.h>
 #define true 0 //true boolean
 #define false 1 // for  false the boolean
+struct product
+	{
+		char id[10];   // product code/no.
+		char name[20]; // product name
+		int  quantity; // remaining quantity of product. Subtract from the original quantity the quantity purchased
+        int  numSold;  // initially zero, when no purchase yet.
+		float price;   // price of one piece of product
+		int discount;  // discount for this product
+		float sales;   // accumulated sales, total sales for this product
+	};
